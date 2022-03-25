9c44b6c59b51468435a0ba59e97b6f2692688add
yushi0620honda <japan20jun1996@gmail.com>
2022-03-26 05:30:39 +0900

    10章まで完了

--- src/main/java/com/example/demo/controller/SignupController.java
+++ src/main/java/com/example/demo/controller/SignupController.java
@@ -4,10 +4,13 @@ import java.util.Locale;
 import java.util.Map;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,5 +55,14 @@ public class SignupController {
 		userService.signup(user);
 		
 		return "redirect:/login";
+	}
+	
+	@ExceptionHandler(DataAccessException.class)
+	public String dataAccessExceptionHandler(DataAccessException e,Model model ) {
+		model.addAttribute("error", "");
+		model.addAttribute("message", "SignupControllerで例外が発生しました");
+		model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR);
+		
+		return "error";
 	}
 }
